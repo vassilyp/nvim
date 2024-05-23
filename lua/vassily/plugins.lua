6e0d1567c870aede1609c559f43098cfc536a3cd
Vassily Petrousevitch <90280831+Vassily-Petrousevitch@users.noreply.github.com>
2024-05-23 15:21:46 -0700

    new colorscheme

--- lua/vassily/plugins.lua
+++ lua/vassily/plugins.lua
@@ -22,14 +22,10 @@ require("lazy").setup({
 	{'akinsho/toggleterm.nvim', version = "*", config = true},
 
 	-- Colorscheme
-	{ 'folke/tokyonight.nvim',
-		priority = 1000, -- Make sure to load this before all the other start plugins.
+	{ 'navarasu/onedark.nvim', priority = 1000,
 		init = function()
-			vim.cmd.colorscheme 'tokyonight-night'
-
-			-- You can configure highlights by doing something like:
-			vim.cmd.hi 'Comment gui=none'
-		end,
+			vim.cmd.colorscheme 'onedark'
+		end
 	},
 
 	{
