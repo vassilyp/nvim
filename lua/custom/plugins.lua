29fdc00f262febe8a7334a550238979f419a26eb
Vassily-Petrousevitch <vasspetrousevitch@gmail.com>
2024-06-22 00:40:49 -0700

    complete refactor!!!

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -18,164 +18,29 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 
-    -- Easily toggle floating terminal
-    {'akinsho/toggleterm.nvim', version = "*", config = true},
+    require 'custom/plugins/toggleterm',
 
-    -- Colorscheme
-    { 'navarasu/onedark.nvim', priority = 1000,
-        init = function()
-            vim.cmd.colorscheme 'onedark'
-        end
-    },
+    require 'custom/plugins/colorscheme',
 
-    {
-        'windwp/nvim-autopairs',
-        event = "InsertEnter",
-        config = true
-        -- use opts = {} for passing setup options
-        -- this is equalent to setup({}) function
-    },
+    require 'custom/plugins/autopairs',
 
-    -- Git stuff on command line
-    { "tpope/vim-fugitive", cmd = "Git" },
-
-    -- "gc" to comment visual regions/lines, gcc to toggle line comment
-    { "numToStr/Comment.nvim", opts = {} },
+    require 'custom/plugins/comment',
 
     require 'custom/plugins/gitsigns',
 
-    { -- Autocompletion
-        "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
-        dependencies = {
-            -- Snippet Engine & its associated nvim-cmp source
-            {
-                "L3MON4D3/LuaSnip",
-                build = (function()
-                    -- Build Step is needed for regex support in snippets.
-                    -- This step is not supported in many windows environments.
-                    -- Remove the below condition to re-enable on windows.
-                    if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-                        return
-                    end
-                    return "make install_jsregexp"
-                end)(),
-                dependencies = {
-                    -- `friendly-snippets` contains a variety of premade snippets.
-                    --    See the README about individual language/framework/plugin snippets:
-                    --    https://github.com/rafamadriz/friendly-snippets
-                    -- {
-                    --   'rafamadriz/friendly-snippets',
-                    --   config = function()
-                    --     require('luasnip.loaders.from_vscode').lazy_load()
-                    --   end,
-                    -- },
-                },
-            },
-            "saadparwaiz1/cmp_luasnip",
+    require 'custom/plugins/nvim-cmp',
 
-            -- Adds other completion capabilities.
-            --  nvim-cmp does not ship with all sources by default. They are split
-            --  into multiple repos for maintenance purposes.
-            "hrsh7th/cmp-nvim-lsp",
-        },
-    },
+    require 'custom/plugins/which-key',
 
-    -- Useful plugin to show you pending keybinds.
-    {
-        "folke/which-key.nvim",
-        event = "VimEnter", -- Sets the loading event to 'VimEnter'
-        config = function() -- This is the function that runs, AFTER loading
-            require("which-key").setup()
+    require 'custom/plugins/telescope',
 
-            -- Document existing key chains
-            require("which-key").register({
-                ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-                ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-                ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-                ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-                ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-            })
-        end,
-    },
+    require 'custom/plugins/lspconfig',
 
-    { -- Fuzzy Finder (files, lsp, etc)
-        "nvim-telescope/telescope.nvim",
-        event = "VimEnter",
-        branch = "0.1.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            { -- If encountering errors, see telescope-fzf-native README for installation instructions
-                "nvim-telescope/telescope-fzf-native.nvim",
-                -- `build` is used to run a command when the plugin is installed/updated.
-                -- This is only run then, not every time Neovim starts up.
-                build = "make",
-
-                -- `cond` is a condition used to determine whether this plugin should be
-                -- installed and loaded.
-                cond = function()
-                    return vim.fn.executable("make") == 1
-                end,
-            },
-            { "nvim-telescope/telescope-ui-select.nvim" },
-            -- Useful for getting pretty icons, but requires a Nerd Font.
-            { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
-        },
-    },
-
-    { -- LSP Configuration & Plugins
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            -- Automatically install LSPs and related tools to stdpath for Neovim
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-            "WhoIsSethDaniel/mason-tool-installer.nvim",
-
-            -- Useful status updates for LSP.
-            -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-            { "j-hui/fidget.nvim", opts = {} },
-
-            -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
-            -- used for completion, annotations and signatures of Neovim apis
-            { "folke/neodev.nvim", opts = {} },
-        },
-    },
-
-    { -- Highlight, edit, and navigate code
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        opts = {
-            ensure_installed = { "bash", "c", "html", "lua", "markdown", "vim", "vimdoc" },
-            -- Autoinstall languages that are not installed
-            auto_install = true,
-            highlight = {
-                enable = true,
-                -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-                --  If you are experiencing weird indenting issues, add the language to
-                --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-                additional_vim_regex_highlighting = { "ruby" },
-            },
-            indent = { enable = true, disable = { "ruby" } },
-        },
-        config = function(_, opts)
-            -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-
-            ---@diagnostic disable-next-line: missing-fields
-            require("nvim-treesitter.configs").setup(opts)
-        end,
-    },
-
-    -- Highlight todo, notes, etc in comments
-    {
-        "folke/todo-comments.nvim",
-        event = "VimEnter",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        opts = { signs = false },
-    },
+    require 'custom/plugins/treesitter',
 
 
-    { -- Collection of various small independent plugins/modules
-        "echasnovski/mini.nvim"
-    },
+    require 'custom/plugins/todo-comments',
+
+    require 'custom/plugins/mini',
 
 })
