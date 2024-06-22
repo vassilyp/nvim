846083d53e6b1e7863d169b4a95bfa4a89af26ab
Vassily-Petrousevitch <vasspetrousevitch@gmail.com>
2024-06-22 01:34:20 -0700

    I'll try using C-y for now

--- lua/custom/plugins/nvim-cmp.lua
+++ lua/custom/plugins/nvim-cmp.lua
@@ -66,8 +66,7 @@ return {
                     -- Accept ([y]es) the completion.
                     --  This will auto-import if your LSP supports it.
                     --  This will expand snippets if the LSP sent a snippet.
-                    -- ["<C-y>"] = cmp.mapping.confirm({ select = true }),    -- old option
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
 
                     -- Manually trigger a completion from nvim-cmp.
                     --  Generally you don't need this, because nvim-cmp will display
