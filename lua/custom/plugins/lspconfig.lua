c4426c903cc34bd29196ba7fadcc3fb0bc85be98
Vassily-Petrousevitch <vasspetrousevitch@gmail.com>
2024-07-04 22:32:02 -0700

    enable go lsp

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -110,7 +110,7 @@ return {
             local servers = {
                 yamlls = {},
                 -- clangd = {},
-                -- gopls = {},
+                gopls = {},
                 -- pyright = {},
                 -- rust_analyzer = {},
                 -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
