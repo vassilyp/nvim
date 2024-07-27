2d40b105b84e45c31f5b43eea2642996f1c8fa7d
Vassily P <vasspetrousevitch@gmail.com>
2024-07-27 12:07:46 -0700

    more indent settings + disable cmp in comments

---
+++ after/ftplugin/go.lua
@@ -0,0 +1,5 @@
+-- Indenting settings
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+vim.opt.softtabstop = 0
--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -145,6 +145,16 @@ return {
             completion = {
                completeopt = 'menu,menuone,noinsert'
             },
+            enabled = function()
+               -- disable completion in comments
+               local context = require 'cmp.config.context'
+               if vim.api.nvim_get_mode().mode == 'c' then
+                  return true
+               else
+                  return not context.in_treesitter_capture("comment")
+                      and not context.in_syntax_group("Comment")
+               end
+            end,
          })
       end
    },
