fd494498e845e02448318916a9150d3f55ef7a86
Vassily P <vasspetrousevitch@gmail.com>
2025-07-06 20:18:53 -0700

    minor changes

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -5,8 +5,14 @@
 -- Current functionality:
 -- CTRL+] to go to definition (CTRL+T to go back)
 -- K for hover signature
+-- grn for LSP rename
+-- gra for LSP code action
+-- grr for LSP references (?)
+-- gri for LSP implementation (?)
+-- gO for LSP document symbol (?)
+-- CTRL-S for signature help while in insert mode
+--
 -- <leader>e for float error (from keymaps)
 -- <leader>q for quickfix list (from keymaps)
-
 
 vim.lsp.enable("lua_ls")
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,6 +1,3 @@
 return {
    "neovim/nvim-lspconfig",
-   -- config = function()
-   --    vim.lsp.enable('lua_ls')
-   -- end
 }
