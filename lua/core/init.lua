7bd7aadfc4fbaa362fd2014b13448c5d26f4801c
Vassily P <vasspetrousevitch@gmail.com>
2025-07-07 10:46:17 -0700

    formatting

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,4 +1,4 @@
-require("core.settings")     -- This goes first because it sets the leader keys
+require("core.settings") -- This goes first because it sets the leader keys
 require("core.keymaps")
 require("core.autocommands")
 require("core.lazy")
