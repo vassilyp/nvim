b2ab02d5afc5a9ddbb4884811d0a6457ab51e50e
Vassily Petrousevitch <vasspetrousevitch@gmail.com>
2025-10-14 17:49:11 -0700

    added everforest for omarchy theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,22 @@
 return {
 	-- Colorscheme
+	-- {
+	-- 	"navarasu/onedark.nvim",
+	-- 	priority = 1000,
+	-- 	init = function()
+	-- 		vim.cmd.colorscheme("onedark")
+	-- 	end,
+	-- },
 	{
-		"navarasu/onedark.nvim",
-		priority = 1000,
-		init = function()
-			vim.cmd.colorscheme("onedark")
+		"neanias/everforest-nvim",
+		version = false,
+		lazy = false,
+		priority = 1000, -- make sure to load this before all the other start plugins
+		-- Optional; default configuration will be used if setup isn't called.
+		config = function()
+			require("everforest").setup({
+				vim.cmd.colorscheme("everforest"),
+			})
 		end,
 	},
 }
