5ffb4aeefb053e6202f0db1cf5aab48f313b09f3
Vassily Petrousevitch <vasspetrousevitch@gmail.com>
2025-12-30 19:00:18 -0800

    auto load plugins in lua/plugins

--- init.lua
+++ init.lua
@@ -3,4 +3,21 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 require("core") -- general/global type settings
-require("plugins")
+
+-- Lazy Plugin Manager
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "--branch=stable",
+        lazyrepo,
+        lazypath,
+    })
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
