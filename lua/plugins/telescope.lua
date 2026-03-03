357d7943daf847aaa650a613bd877b4026eeb3b5
Vassily Petrousevitch <vassily.petrousevitch@zipcontractors.com>
2026-03-02 17:35:18 -0800

    new telescope + treesitter config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,24 +1,18 @@
 return {
-    { -- Fuzzy Finder (files, lsp, etc)
+    {
         "nvim-telescope/telescope.nvim",
         event = "VimEnter",
-        branch = "0.1.x",
         dependencies = {
             "nvim-lua/plenary.nvim",
-            { -- If encountering errors, see telescope-fzf-native README for installation instructions
+            {
                 "nvim-telescope/telescope-fzf-native.nvim",
-                -- `build` is used to run a command when the plugin is installed/updated.
-                -- This is only run then, not every time Neovim starts up.
                 build = "make",
 
-                -- `cond` is a condition used to determine whether this plugin should be
-                -- installed and loaded.
                 cond = function()
                     return vim.fn.executable("make") == 1
                 end,
             },
             { "nvim-telescope/telescope-ui-select.nvim" },
-            -- Useful for getting pretty icons, but requires a Nerd Font.
             {
                 "nvim-tree/nvim-web-devicons",
                 enabled = vim.g.have_nerd_font,
@@ -29,7 +23,6 @@ return {
             local themes = require("telescope.themes")
 
             telescope.setup({
-                -- See :help telescope.setup() for default mappings/updates/etc
                 pickers = {
                     find_files = {
                         find_command = {
@@ -58,7 +51,6 @@ return {
             pcall(telescope.load_extension, "fzf")
             pcall(telescope.load_extension, "ui-select")
 
-            -- See `:help telescope.builtin`
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
             vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
@@ -70,23 +62,6 @@ return {
             vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
             vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
             vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-
-            vim.keymap.set("n", "<leader>/", function()
-                builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-                    winblend = 10,
-                    previewer = false,
-                }))
-            end, { desc = "[/] Fuzzily search in current buffer" })
-
-            --  See `:help telescope.builtin.live_grep()` for information about particular keys
-            vim.keymap.set("n", "<leader>s/", function()
-                builtin.live_grep({
-                    grep_open_files = true,
-                    prompt_title = "Live Grep in Open Files",
-                })
-            end, { desc = "[S]earch [/] in Open Files" })
-
-            -- Shortcut for searching your Neovim configuration files
             vim.keymap.set("n", "<leader>sn", function()
                 builtin.find_files({ cwd = vim.fn.stdpath("config") })
             end, { desc = "[S]earch [N]eovim files" })
